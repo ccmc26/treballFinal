ce714b4d0f4d67af8bef6990f29875a680c3143c
carconex <karmeconemar@gmail.com>
2024-03-24 13:45:43 +0100

    cambios

---
+++ app/Models/Articulo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'articulo';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'stock',
+        'discount',
+        'photo'
+    ];
+}
