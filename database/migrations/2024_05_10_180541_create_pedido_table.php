3a43f4bc9de6f1dc9cd1a8be1f672689651117c6
carconex <karmeconemar@gmail.com>
2024-05-11 09:09:56 +0200

    modificació de la migration pedido

--- database/migrations/2024_05_10_180541_create_pedido_table.php
+++ database/migrations/2024_05_10_180541_create_pedido_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,14 @@ return new class extends Migration
     {
         Schema::create('pedido', function (Blueprint $table) {
             $table->id();
+
+            $table->decimal('Totalprice');
+            $table->string('status');
+
             $table->timestamps();
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable();
+
         });
     }
 
